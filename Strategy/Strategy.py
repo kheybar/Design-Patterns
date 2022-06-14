93cc0007a392bc2660b4cd8139c3a916401484e9
mahdi zarepour <mahdizarepour15@gmail.com>
2022-06-14 10:30:09 +0430

    strategy pattern

--- Strategy/Strategy.py
+++ Strategy/Strategy.py
@@ -1,45 +1,55 @@
-"""
-
-    Strategy
-
-"""
 from abc import ABC, abstractmethod
 
 
-
-class Context:
-    def __init__(self, sentence, direction):
-        self.sentence = sentence
-        self._direction = direction
-
-    @property
-    def direction(self):
-        self._direction = dir
-    
-    @direction.setter
-    def direction(self, dir):
-        self._direction = dir
-
-    def sorting(self):
-        return self._direction.direct(self.sentence)
-
-
-
-class Direction(ABC):
+class Compressor(ABC):
+    """Interface"""
     @abstractmethod
-    def direct(self, data):
+    def algorithm(self):
         pass
 
 
-class Right(Direction):
-    def direct(self, data):
-        print(data[::-1])
-
-class Left(Direction):
-    def direct(self, data):
-        print(data)
+class Filter(ABC):
+    """Interface"""
+    @abstractmethod
+    def type(self):
+        pass
 
 
+class RAWCompressor(Compressor):
+    """Concrete Compressor"""
+    def algorithm(self):
+        return 'RAW compression'
 
-c1 = Context('Hello World ...', Right())
-c1.sorting()
+
+class JPGCompressor(Compressor):
+    """Concrete Compressor"""
+    def algorithm(self):
+        return 'JPG compression'
+
+
+class WBFilter(Filter):
+    """Concrete Filter"""
+    def type(self):
+        return 'W&B filter'
+
+
+class HighContrastFilter(Filter):
+    """Concrete Filter"""
+    def type(self):
+        return 'High Contrast filter'
+
+
+class ImageStore:
+    def __init__(self, filename, compressor:Compressor, filter:Filter):
+        self.filename = filename
+        self.compressor = compressor
+        self.filter = filter
+
+    def store(self):
+        return f'{self.filename}-{self.compressor().algorithm()}-{self.filter().type()}'
+
+
+
+a = ImageStore('test', JPGCompressor, HighContrastFilter)
+result = a.store()
+print(result)
